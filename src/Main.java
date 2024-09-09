60d802bb13ef96a10213d494ee6ce360b7dbc971
dtseukr <hortyong@gmail.com>
2024-09-09 16:14:33 +0200

    Додав рядок для виведення поточної дати на екран

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {System.out.printf("Hello and welcome! This task done by Tkachenko Maxym Pz-21-1/9 ");
+        LocalDate currentDate = LocalDate.now();
+        System.out.printf("Current date: %s\n", currentDate);
     }
 }
